4a056a6239829f39e4980e9826ab77d493ec816a
RahulSP-4601 <rahulsp4601@gmail.com>
2024-09-16 15:12:47 -0500

    BA1.py
    Committing Bonus Class Assignment

---
+++ BA1.py
@@ -0,0 +1,24 @@
+"""
+  Team ID: 6
+        Name                    ID
+  Rahul Sanjay Panchal      1002147137
+  Sejal Prithviraj Chavan   1002152342
+  Ashritha varma pamudurthy 1002165358
+  Harsha Potluri            1002162450
+  
+"""
+# calculator.py
+def add(x, y):
+"""Addition"""
+return x + y
+def subtract(x, y):
+"""Subtraction"""
+return x - y
+def multiply(x, y):
+"""Multiplication"""
+return x * y
+def divide(x, y):
+"""Division"""
+if y == 0:
+raise ValueError("Cannot divide by zero!")
+return x / y
